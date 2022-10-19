60717ecfb68ee800d1b2eb8262a7cf134d47d74a
Vivek Verma <bookyourown.mutual@gmail.com>
2022-10-19 09:28:58 +0530

    vhang

--- services.php
+++ services.php
@@ -6,12 +6,16 @@
 			
 			<div class="welcome services-grid">
 				<div class="welcome-left offer-left services-left">
+
 					<h3>Services</h3>
 					<p style="color: black;">We provide services to suit demands of construction and mining industry:</p>
-					<p style="color: black;"><strong>1. </strong>Repair and maintenance of equipments</p>
+					
+					<p style="color: black;"><strong>1. </strong>Repair and maintenance services for Excavator, Dozer, Grader, Pay loader, Tipper/Dump Trucks & Surface Miner.</p>
 					<p style="color: black;"><strong>2. </strong>Tipper body and Bucket repairing services</p>
-					<p style="color: black;"><strong>3. </strong>Track chain overhauping services</p>
-					<p style="color: black;"><strong>4. </strong>Accidental repairing services (Insurance tie ups- <strong>Cash less Service *</strong> )</p>					
+					<p style="color: black;"><strong>3. </strong>Track chain overhaupng services</p>
+					<p style="color: black;"><strong>4. </strong>Accidental repairing services (Insurance tie ups- <strong>Cash less Service *</strong> )</p>
+
+					
 				</div>
 				<div class="welcome-right services-right">
 					
@@ -21,7 +25,7 @@
 			<div class="welcome new services-grid">
 				<div class="welcome-left new-left services-left">
 					<h3>Products</h3>
-					<p style="color: black;"> We are also in manufacturing and development of custom built products such as;</p>
+					<p style="color: black;">We are also in manufacturing and development of custom built products such as;</p>
 					<p style="color: black;"><strong>1. </strong> Workshop On Wheels (Fully Customizable Mobile Workshop)</p>
 					<p style="color: black;"><strong>2. </strong> Service Van</p>
 					<p style="color: black;"><strong>3. </strong> Recon Solutions</p>
@@ -36,33 +40,33 @@
 		<div class="services-bottom">
 			<!-- container -->
 			<div class="container">
-				<!-- <div class="services-bottom-info">
+				<div class="services-bottom-info">
 					<h3>Deep Mining</h3>
-				</div> -->
+				</div>
 				<div class="services-bottom-grids">
 					<div class=" col-md-4 services-bottom-grid">
 						<div class="services-img">
 							<img src="images/s1.png" alt="" />
 						</div>
 						<h5>Efficient Service</h5>
-						<!-- <p>We provide efficient service
-						</p> -->
+						<p>We provide efficient service
+						</p>
 					</div>
 					<div class=" col-md-4 services-bottom-grid">
 						<div class="services-img">
 							<img src="images/s2.png" alt="" />
 						</div>
-						<h5>Experienced Manpower</h5>
-						<!-- <p>Great manpower
-						</p> -->
+						<h5>Manpower</h5>
+						<p>Great manpower
+						</p>
 					</div>
 					<div class=" col-md-4 services-bottom-grid">
 						<div class="services-img">
 							<img src="images/s3.png" alt="" />
 						</div>
 						<h5>Equipments</h5>
-						<!-- <p>All sorts of mining equipments available
-						</p> -->
+						<p>All sorts of mining equipments available
+						</p>
 					</div>
 					<div class="clearfix"> </div>
 				</div>
